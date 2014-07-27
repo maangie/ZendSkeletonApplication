77021a414187621fa32a4c7c5a88f30dc0803837
maangie <maangie@users.noreply.github.com>
2014-07-27 14:14:16 +0900

    Controller を作成 「Zend Framework 2徹底解説」03-05 (59-65p.)

---
+++ module/Application/src/Application/Controller/UserController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class UserController extends AbstractActionController
+{
+    public function addAction()
+    {
+        return new ViewModel();
+    }
+}
